650093b731251966cb3ab3b8b96053ea0025ec68
ilnazway <localhost@mail.loc>
2022-08-11 00:07:34 +0300

    vue v-for

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,17 @@
 const { createApp, ref } = Vue;
 
 const test = {
+  setup() {
+    const list = ['cat', 'dog', 'people', 'people'];
+
+    return {
+      list,
+    }
+  },
   template: /*html*/`
-    <div>hellow word</div>
+    <ul>
+      <li v-for="(item, index) of list" :class="index" :key="index">{{index}} = {{ item }}</li>
+    </ul>
   `,
 };
 
