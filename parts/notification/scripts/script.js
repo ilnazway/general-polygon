20dbead812e506f4e6c1168702d8c4b8144a7a2c
ilnazway <localhost@mail.loc>
2022-08-26 11:07:43 +0300

    Мигающая иконка

--- parts/notification/scripts/script.js
+++ parts/notification/scripts/script.js
@@ -4,8 +4,12 @@ outputNotification();
 function outputNotification() {
   let listNewNotif = [];
   let flickerTimer;
+  let flickerTimerIcon;
   let flashingTime = 1000;
   const startTitle = document.title;
+  const icon = document.querySelector('link[rel="icon"]');
+  const urlDefaultIcon = icon.getAttribute('href');
+  const urlIconNotif = './img/icon_songs.ico';
 
   newValueNotification();
 
@@ -46,6 +50,7 @@ function outputNotification() {
 
       } else if (listLength === 1) {
         document.title = listNotification[0];
+        startFlickerIcon();
 
         flickerTimer = setInterval(function() {
           if (count % 2 == 0) {
@@ -58,6 +63,7 @@ function outputNotification() {
 
       } else {
         document.title = listNotification[0];
+        startFlickerIcon();
 
         flickerTimer = setInterval(function() {
           if ((listLength - 1) <= count) {
@@ -74,10 +80,29 @@ function outputNotification() {
     const btnStop = document.querySelector('#stop');
     btnStop.addEventListener('click', function() {
       clearInterval(flickerTimer);
+      clearInterval(flickerTimerIcon);
+
+      icon.setAttribute('href', urlDefaultIcon);
       document.title = startTitle;
       console.log('stop');
     });
   };
+
+  function startFlickerIcon() {
+    let count = 0;
+    icon.setAttribute('href', urlIconNotif);
+
+    flickerTimerIcon = setInterval(function() {
+
+      if (count % 2 === 0) {
+        icon.setAttribute('href', urlDefaultIcon);
+      } else {
+        icon.setAttribute('href', urlIconNotif);
+      }
+      count++;
+
+    }, flashingTime);
+  }
 };
 
 
